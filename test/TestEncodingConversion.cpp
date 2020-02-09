88159eb5c2dfb256ceb9d4a3b9d3c59cfd651960
Antoine <end2endzone@gmail.com>
2020-02-09 17:58:07 -0500

    Added more functions to detect possible encodings.

--- test/TestEncodingConversion.cpp
+++ test/TestEncodingConversion.cpp
@@ -41,6 +41,50 @@ namespace win32clipboard { namespace test
   {
   }
   //--------------------------------------------------------------------------------------------------
+  TEST_F(TestEncodingConversion, testIsAscii)
+  {
+    ASSERT_TRUE ( win32clipboard::is_ascii("foobar") );
+    ASSERT_TRUE ( win32clipboard::is_ascii("\b\t\n\r\\\"!/$%?&*()_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+
+    ASSERT_FALSE( win32clipboard::is_ascii("école") );   //school in french
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestEncodingConversion, testIsCp1252Valid)
+  {
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("foobar") );
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("\\\b\t\n\r\"")); //control characters
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("español") );     //spanish
+    ASSERT_TRUE ( win32clipboard::is_cp1252_valid("école") );       //school in french
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestEncodingConversion, testIsIso8859_1)
+  {
+    ASSERT_TRUE ( win32clipboard::is_iso8859_1_valid("foobar") );
+    ASSERT_TRUE ( win32clipboard::is_iso8859_1_valid("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( win32clipboard::is_iso8859_1_valid("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+    ASSERT_TRUE ( win32clipboard::is_iso8859_1_valid("español") );     //spanish
+    ASSERT_TRUE ( win32clipboard::is_iso8859_1_valid("école") );       //school in french
+
+    ASSERT_FALSE( win32clipboard::is_iso8859_1_valid("\\\b\t\n\r\"")); //control characters
+    ASSERT_FALSE( win32clipboard::is_iso8859_1_valid("\x0d\x0a") );   //CRLF, ISO-8859-1 have no control character
+  }
+  //--------------------------------------------------------------------------------------------------
+  TEST_F(TestEncodingConversion, testIsUtf8)
+  {
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("foobar") );
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("\\\b\t\n\r\"")); //control characters
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("!/$%?&*()_+"));  //symbols
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") );
+
+    //accent letters from Windows CP1252 encoding
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("espa" "\xC3\xB1" "ol") );  //spanish, U+00F1
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("\xC3\xA9" "cole") );       //school in french
+
+    ASSERT_TRUE ( win32clipboard::is_utf8_valid("\x0d\x0a") );    //CRLF
+  }
+  //--------------------------------------------------------------------------------------------------
   TEST_F(TestEncodingConversion, testAnsiUnicode)
   {
     const std::string str_ansi = "école"; //school in french
